6d37b732f423d98ed80779138386e1a7f94577d4
steffzilla@mail.com <steffzilla@mail.com>
2024-12-06 21:35:22 +0100

    Added Test for day 6 based on a JSON file with the correct solutions for my AdventOfCode Account. The correct solutions are not contained in this repo.

---
+++ src/test/java/de/steffzilla/aoc/y2024/Aoc2024_06Test.java
@@ -0,0 +1,47 @@
+package de.steffzilla.aoc.y2024;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.steffzilla.aoc.AocUtils;
+import org.javatuples.Pair;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.Assert.*;
+
+public class Aoc2024_06Test {
+
+    static final String sample = """
+            ....#.....
+            .........#
+            ..........
+            ..#.......
+            .......#..
+            ..........
+            .#..^.....
+            ........#.
+            #.........
+            ......#...
+            """;
+
+    @Test
+    public void testSolveExample() {
+        Pair<String, String> solutions = Aoc2024_06.solve(sample.lines().toList());
+        assertEquals("41", solutions.getValue0());
+        assertEquals("6", solutions.getValue1());
+    }
+
+    @Test
+    public void testSolvePuzzle() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("solutions.json");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(inputStream);
+
+        Pair<String, String> solutions = Aoc2024_06.solve(AocUtils.getStringList(Aoc2024_06.PATH));
+        assertEquals(root.at("/adventOfCode/2024/day6/part1").asText(), solutions.getValue0());
+        assertEquals(root.at("/adventOfCode/2024/day6/part2").asText(), solutions.getValue1());
+    }
+
+}
