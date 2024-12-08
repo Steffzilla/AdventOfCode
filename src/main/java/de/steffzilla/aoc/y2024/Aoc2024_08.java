9415456eb1d94d775cde8564f22c6ba196b4ad70
steffzilla@mail.com <steffzilla@mail.com>
2024-12-08 16:55:10 +0100

    Added solution and tests for day 8 of 2024

---
+++ src/main/java/de/steffzilla/aoc/y2024/Aoc2024_08.java
@@ -0,0 +1,141 @@
+package de.steffzilla.aoc.y2024;
+
+import de.steffzilla.aoc.AocUtils;
+import de.steffzilla.aoc.CharacterField;
+import org.javatuples.Pair;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class Aoc2024_08 {
+
+    private static final String DAY = "08";
+    private static final String YEAR = "2024";
+    private static final String USERNAME = System.getProperty("user.name");
+    public static final String BASEDIR = "C://Users//" + USERNAME + "//Downloads//AoC" + YEAR + "//";
+    public static final String FILENAME = "input"+YEAR+"_"+DAY+".txt";
+    //public static final String FILENAME = "sample" + YEAR + "_" + DAY + ".txt";
+    public static final String PATH = BASEDIR + FILENAME;
+
+    static final String example = """
+            T.........
+            ...T......
+            .T........
+            ..........
+            ..........
+            ..........
+            ..........
+            ..........
+            ..........
+            ..........
+            """;
+
+    public static void main(String[] args) {
+        System.out.println(DAY + ".12." + YEAR);
+        //List<String> inputLines = example.lines().toList();
+        List<String> inputLines = AocUtils.getStringList(PATH);
+
+        solve(inputLines);
+    }
+
+    static Pair<String, String> solve(List<String> inputLines) {
+        CharacterField cf = new CharacterField(inputLines);
+        return new Pair<>(part1(cf), part2(cf));
+    }
+
+    private static String part1(CharacterField cf) {
+        HashMap<String, List<Pair<Integer, Integer>>> antennaPositions = readPositions(cf);
+        HashSet<Pair<Integer, Integer>> antinodePositions = new HashSet<>();
+
+        for (String frequency : antennaPositions.keySet()) {
+            System.out.print(frequency + ": ");
+            List<Pair<Integer, Integer>> pairs = antennaPositions.get(frequency);
+            System.out.println(pairs);
+            for (int outerIter = 0; outerIter < pairs.size(); outerIter++) {
+                for (int innerIter = outerIter + 1; innerIter < pairs.size(); innerIter++) {
+                    Pair<Integer, Integer> first = pairs.get(outerIter);
+                    Pair<Integer, Integer> second = pairs.get(innerIter);
+                    int x = first.getValue0() + first.getValue0() - second.getValue0();
+                    int y = first.getValue1() + first.getValue1() - second.getValue1();
+                    if (cf.isContained(x, y)) {
+                        antinodePositions.add(new Pair<>(x, y));
+                    }
+                    x = second.getValue0() + second.getValue0() - first.getValue0();
+                    y = second.getValue1() + second.getValue1() - first.getValue1();
+                    if (cf.isContained(x, y)) {
+                        antinodePositions.add(new Pair<>(x, y));
+                    }
+                }
+            }
+        }
+
+        System.out.println("\nPart 1 > Result: " + antinodePositions.size());
+        return String.valueOf(antinodePositions.size());
+    }
+
+    private static HashMap<String, List<Pair<Integer, Integer>>> readPositions(CharacterField cf) {
+        HashMap<String, List<Pair<Integer, Integer>>> antennaPositions = new HashMap<>();
+        for (int y = 0; y < cf.getMaxY(); y++) {
+            for (int x = 0; x < cf.getMaxX(); x++) {
+                String frequency = cf.getCharacterAt(x, y);
+                if (!frequency.equals(".")) {
+                    List<Pair<Integer, Integer>> positions = antennaPositions.get(frequency);
+                    if (positions == null) {
+                        positions = new ArrayList<>();
+                        antennaPositions.put(frequency, positions);
+                    }
+                    positions.add(new Pair<>(x, y));
+                }
+            }
+        }
+        return antennaPositions;
+    }
+
+    private static String part2(CharacterField cf) {
+        HashMap<String, List<Pair<Integer, Integer>>> antennaPositions = readPositions(cf);
+        HashSet<Pair<Integer, Integer>> antinodePositions = new HashSet<>();
+
+        for (String frequency : antennaPositions.keySet()) {
+            System.out.print(frequency + ": ");
+            List<Pair<Integer, Integer>> pairs = antennaPositions.get(frequency);
+            System.out.println(pairs);
+            for (int outerIter = 0; outerIter < pairs.size(); outerIter++) {
+                for (int innerIter = outerIter + 1; innerIter < pairs.size(); innerIter++) {
+                    Pair<Integer, Integer> first = pairs.get(outerIter);
+                    Pair<Integer, Integer> second = pairs.get(innerIter);
+                    boolean endOfField = false;
+                    int factor = 0;
+                    while (!endOfField) {
+                        int x = first.getValue0() + factor * (first.getValue0() - second.getValue0());
+                        int y = first.getValue1() + factor * (first.getValue1() - second.getValue1());
+                        if (cf.isContained(x, y)) {
+                            antinodePositions.add(new Pair<>(x, y));
+                        } else {
+                            endOfField = true;
+                        }
+                        factor++;
+                    }
+
+                    endOfField = false;
+                    factor = 0;
+                    while (!endOfField) {
+                        int x = second.getValue0() + factor * (second.getValue0() - first.getValue0());
+                        int y = second.getValue1() + factor * (second.getValue1() - first.getValue1());
+                        if (cf.isContained(x, y)) {
+                            antinodePositions.add(new Pair<>(x, y));
+                        } else {
+                            endOfField = true;
+                        }
+                        factor++;
+                    }
+                }
+            }
+        }
+
+        System.out.println("\nPart 2 > Result: " + antinodePositions.size());
+        return String.valueOf(antinodePositions.size());
+    }
+
+}
---
+++ src/test/java/de/steffzilla/aoc/y2024/Aoc2024_08Test.java
@@ -0,0 +1,49 @@
+package de.steffzilla.aoc.y2024;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.steffzilla.aoc.AocUtils;
+import org.javatuples.Pair;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class Aoc2024_08Test {
+
+    static final String sample = """
+            ............
+            ........0...
+            .....0......
+            .......0....
+            ....0.......
+            ......A.....
+            ............
+            ............
+            ........A...
+            .........A..
+            ............
+            ............
+            """;
+
+    @Test
+    public void testSolveExample() {
+        Pair<String, String> solutions = Aoc2024_08.solve(sample.lines().toList());
+        assertEquals("14", solutions.getValue0());
+        assertEquals("34", solutions.getValue1());
+    }
+
+    @Test
+    public void testSolvePuzzle() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("solutions.json");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(inputStream);
+
+        Pair<String, String> solutions = Aoc2024_08.solve(AocUtils.getStringList(Aoc2024_08.PATH));
+        assertEquals(root.at("/adventOfCode/2024/day8/part1").asText(), solutions.getValue0());
+        assertEquals(root.at("/adventOfCode/2024/day8/part2").asText(), solutions.getValue1());
+    }
+
+}
