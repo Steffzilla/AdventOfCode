c8d4d34e642f0564d89dbc68125280583e50229d
steffzilla <steffzilla@mail.com>
2024-12-10 17:35:36 +0100

    Added Part 2 Tests for day 9 Added JGraphT and updated Apache Commons Lang3 and Guava

--- src/test/java/de/steffzilla/aoc/y2024/Aoc2024_09Test.java
+++ src/test/java/de/steffzilla/aoc/y2024/Aoc2024_09Test.java
@@ -19,7 +19,7 @@ public class Aoc2024_09Test {
     public void testSolveExample() {
         Pair<String, String> solutions = Aoc2024_09.solve(sample.lines().toList());
         assertEquals("1928", solutions.getValue0());
-        //assertEquals("2858", solutions.getValue1());
+        assertEquals("2858", solutions.getValue1());
     }
 
     @Test
@@ -30,7 +30,7 @@ public class Aoc2024_09Test {
 
         Pair<String, String> solutions = Aoc2024_09.solve(AocUtils.getStringList(Aoc2024_09.PATH));
         assertEquals(root.at("/adventOfCode/2024/day9/part1").asText(), solutions.getValue0());
-        //assertEquals(root.at("/adventOfCode/2024/day9/part2").asText(), solutions.getValue1());
+        assertEquals(root.at("/adventOfCode/2024/day9/part2").asText(), solutions.getValue1());
     }
 
 }
