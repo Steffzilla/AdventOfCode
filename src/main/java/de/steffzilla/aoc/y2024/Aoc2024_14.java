f2d4cc25798b42f37d681a7f62410bfecb516165
steffzilla@mail.com <steffzilla@mail.com>
2024-12-15 08:18:12 +0100

    Added a solution and a test class for day 14 2024

---
+++ src/main/java/de/steffzilla/aoc/y2024/Aoc2024_14.java
@@ -0,0 +1,156 @@
+package de.steffzilla.aoc.y2024;
+
+import de.steffzilla.aoc.AocUtils;
+import de.steffzilla.aoc.CharacterField;
+import de.steffzilla.aoc.InputUtils;
+import org.javatuples.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aoc2024_14 {
+
+    private static final String DAY = "14";
+    private static final String YEAR = "2024";
+    private static final String USERNAME = System.getProperty("user.name");
+    public static final String BASEDIR = "C://Users//" + USERNAME + "//Downloads//AoC" + YEAR + "//";
+    public static final String FILENAME = "input" + YEAR + "_" + DAY + ".txt";
+    //public static final String FILENAME = "sample" + YEAR + "_" + DAY + ".txt";
+    public static final String PATH = BASEDIR + FILENAME;
+
+    static int part1Moves = 100;
+    static int part2Moves = 100000;
+    static int fieldDimensionX = 101;
+    static int fieldDimensionY = 103;
+    static List<Robot> robots;
+
+    public static void main(String[] args) {
+        System.out.println(DAY + ".12." + YEAR);
+        List<String> inputLines = AocUtils.getStringList(PATH);
+
+        solve(inputLines);
+    }
+
+    static Pair<String, String> solve(List<String> inputLines) {
+        List<List<Integer>> inputData = InputUtils.readLinesAsInts("^p=(-?\\d*),(-?\\d*) v=(-?\\d*),(-?\\d*)", inputLines);
+        return new Pair<>(part1(inputData), part2(inputData));
+    }
+
+    private static String part1(List<List<Integer>> inputLines) {
+        setupInitialRobotState(inputLines);
+        for (int i = 0; i < part1Moves; i++) {
+            moveRobots(robots);
+        }
+        // In which quadrant are the robots
+        long safetyFactor = calculateSafetyFactor(robots);
+        System.out.println("\nPart 1 > Result: " + safetyFactor);
+        return String.valueOf(safetyFactor);
+    }
+
+    private static void setupInitialRobotState(List<List<Integer>> inputLines) {
+        robots = new ArrayList<>();
+        for (List<Integer> line : inputLines) {
+            Robot robot = new Robot(line.get(0), line.get(1), line.get(2), line.get(3));
+            robots.add(robot);
+        }
+    }
+
+    private static long calculateSafetyFactor(List<Robot> robots) {
+        long q1 = 0;
+        long q2 = 0;
+        long q3 = 0;
+        long q4 = 0;
+        int maxPosQ1X = (fieldDimensionX - 1) / 2 - 1;
+        int maxPosQ1Y = (fieldDimensionY - 1) / 2 - 1;
+        for (Robot robot : robots) {
+            if (robot.posX <= maxPosQ1X) {
+                if (robot.posY <= maxPosQ1Y) {
+                    q1++;
+                } else if (robot.posY > maxPosQ1Y + 1) {
+                    q3++;
+                }
+            } else if (robot.posX > maxPosQ1X + 1) {
+                if (robot.posY <= maxPosQ1Y) {
+                    q2++;
+                } else if (robot.posY > maxPosQ1Y + 1) {
+                    q4++;
+                }
+            }
+        }
+        return q1 * q2 * q3 * q4;
+    }
+
+    private static void moveRobots(List<Robot> robots) {
+        for (Robot robot : robots) {
+            int x = robot.posX + robot.velX;
+            if (x >= fieldDimensionX) { // the max value is out of bounds
+                x -= fieldDimensionX;
+            } else if (x < 0) {
+                x += fieldDimensionX;
+            }
+            robot.posX = x;
+            int y = robot.posY + robot.velY;
+            if (y >= fieldDimensionY) {
+                y -= fieldDimensionY;
+            } else if (y < 0) {
+                y += fieldDimensionY;
+            }
+            robot.posY = y;
+        }
+    }
+
+    private static String part2(List<List<Integer>> inputLines) {
+        long count = 0;
+        setupInitialRobotState(inputLines);
+        for (int i = 1; i <= part2Moves; i++) {
+            moveRobots(robots);
+            if (searchLine(robots)) {
+                count = i;
+                break;
+            }
+        }
+        System.out.println("\nPart 2 > Result: " + count);
+        return String.valueOf(count);
+    }
+
+    private static boolean searchLine(List<Robot> robots) {
+        CharacterField cf = new CharacterField(fieldDimensionX, fieldDimensionY, ".");
+        for (Robot robot : robots) {
+            cf.setCharacterAt("#", robot.posX, robot.posY);
+        }
+        for (int line = 0; line < fieldDimensionY; line++) {
+            if (cf.lineContains(line, "###############################")) {
+                System.out.println();
+                System.out.println();
+                cf.prettyPrint();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static class Robot {
+        int posX;
+        int posY;
+        int velX;
+        int velY;
+
+        public Robot(int posX, int posY, int velX, int velY) {
+            this.posX = posX;
+            this.posY = posY;
+            this.velX = velX;
+            this.velY = velY;
+        }
+
+        @Override
+        public String toString() {
+            return "Robot{" +
+                    "posX=" + posX +
+                    ", posY=" + posY +
+                    ", velX=" + velX +
+                    ", velY=" + velY +
+                    '}';
+        }
+    }
+
+}
---
+++ src/test/java/de/steffzilla/aoc/y2024/Aoc2024_14Test.java
@@ -0,0 +1,94 @@
+package de.steffzilla.aoc.y2024;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.steffzilla.aoc.AocUtils;
+import org.javatuples.Pair;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class Aoc2024_14Test {
+
+    static final String sample = """
+            p=0,4 v=3,-3
+            p=6,3 v=-1,-3
+            p=10,3 v=-1,2
+            p=2,0 v=2,-1
+            p=0,0 v=1,3
+            p=3,0 v=-2,-2
+            p=7,6 v=-1,-3
+            p=3,0 v=-1,-2
+            p=9,3 v=2,3
+            p=7,3 v=-1,2
+            p=2,4 v=2,-3
+            p=9,5 v=-3,-3
+            """;
+
+    static final String test = """
+            p=0,0 v=1,0
+            p=0,1 v=-1,0
+            p=0,3 v=0,1
+            p=0,4 v=0,-1
+            p=1,1 v=2,2
+            p=3,3 v=-2,-2
+            p=5,5 v=0,0
+            """;
+
+    static final String testQ = """
+            p=0,0 v=0,0
+            p=1,1 v=0,0
+            p=10,0 v=0,0
+            p=9,1 v=0,0
+            p=0,6 v=0,0
+            p=1,5 v=0,0
+            p=10,6 v=0,0
+            p=9,5 v=0,0
+            p=5,3 v=0,0
+            """;
+
+    static final String testQExample = """
+            p=0,2 v=0,0
+            p=6,0 v=0,0
+            p=6,0 v=0,0
+            p=9,0 v=0,0
+            p=3,5 v=0,0
+            p=4,5 v=0,0
+            p=4,5 v=0,0
+            p=1,6 v=0,0
+            p=6,6 v=0,0
+            """;
+
+    @Test
+    public void testQuadrants() {
+        Aoc2024_14.fieldDimensionX = 11;
+        Aoc2024_14.fieldDimensionY = 7;
+        Pair<String, String> solutions = Aoc2024_14.solve(testQ.lines().toList());
+        assertEquals("16", solutions.getValue0());
+        solutions = Aoc2024_14.solve(testQExample.lines().toList());
+        assertEquals("12", solutions.getValue0());
+    }
+
+    @Test
+    public void testSolveExample() {
+        Aoc2024_14.fieldDimensionX = 11;
+        Aoc2024_14.fieldDimensionY = 7;
+        Pair<String, String> solutions = Aoc2024_14.solve(sample.lines().toList());
+        assertEquals("12", solutions.getValue0());
+    }
+
+    @Test
+    public void testSolvePuzzle() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("solutions.json");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(inputStream);
+
+        Pair<String, String> solutions = Aoc2024_14.solve(AocUtils.getStringList(Aoc2024_14.PATH));
+        assertEquals(root.at("/adventOfCode/2024/day14/part1").asText(), solutions.getValue0());
+        assertEquals(root.at("/adventOfCode/2024/day14/part2").asText(), solutions.getValue1());
+    }
+
+}
