c50512267960e8b99f54fad3c3273eb7d606b02d
steffzilla@mail.com <steffzilla@mail.com>
2024-12-23 10:28:45 +0100

    Adds a class that represents an edge in a JGrapht graph with a label.

---
+++ src/main/java/de/steffzilla/aoc/LabeledEdge.java
@@ -0,0 +1,24 @@
+package de.steffzilla.aoc;
+
+import org.jgrapht.graph.DefaultEdge;
+
+/**
+ * Edge in a JGrapht graph with a label.
+ * Extends {@link DefaultEdge}.
+ */
+public class LabeledEdge extends DefaultEdge {
+    private final String label;
+
+    public LabeledEdge(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + getSource() + " : " + getTarget() + " : " + label + ")";
+    }
+}
