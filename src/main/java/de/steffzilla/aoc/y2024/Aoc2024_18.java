5ac9fc98a9cb4073526ffa252d93695ebf6da962
steffzilla@mail.com <steffzilla@mail.com>
2024-12-29 14:19:48 +0100

    Added the solution for day 18 plus tests

---
+++ src/main/java/de/steffzilla/aoc/y2024/Aoc2024_18.java
@@ -0,0 +1,104 @@
+package de.steffzilla.aoc.y2024;
+
+import de.steffzilla.aoc.AocUtils;
+import de.steffzilla.aoc.CharacterField;
+import de.steffzilla.aoc.InputUtils;
+import org.javatuples.Pair;
+import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.BFSShortestPath;
+import org.jgrapht.graph.DefaultEdge;
+
+import java.util.List;
+
+
+public class Aoc2024_18 {
+
+    private static final String DAY = "18";
+    private static final String YEAR = "2024";
+    private static final String USERNAME = System.getProperty("user.name");
+    public static final String BASEDIR = "C://Users//" + USERNAME + "//Downloads//AoC" + YEAR + "//";
+    public static final String INPUT_FILENAME = "input" + YEAR + "_" + DAY + ".txt";
+    public static final String SAMPLE_FILENAME = "sample" + YEAR + "_" + DAY + ".txt";
+    public static final String BLOCKING = "#";
+
+    static int maxXCoordinate;
+    static int maxYCoordinate;
+    static int numberOfBytesPart1;
+
+    public static void main(String[] args) {
+        List<String> inputLines = initialize(true);
+        solve(inputLines);
+    }
+
+    static List<String> initialize(boolean solvePuzzle) {
+        List<String> inputLines;
+        if (solvePuzzle) {
+            System.out.println(DAY + ".12." + YEAR + ": Solving the PUZZLE");
+            maxXCoordinate = 70;
+            maxYCoordinate = 70;
+            numberOfBytesPart1 = 1024;
+            inputLines = AocUtils.getStringList(BASEDIR + INPUT_FILENAME);
+        } else {
+            System.out.println(DAY + ".12." + YEAR + ": Solving the example");
+            maxXCoordinate = 6;
+            maxYCoordinate = 6;
+            numberOfBytesPart1 = 12;
+            inputLines = AocUtils.getStringList(BASEDIR + SAMPLE_FILENAME);
+        }
+        return inputLines;
+    }
+
+    static Pair<String, String> solve(List<String> inputLines) {
+        return new Pair<>(part1(inputLines), part2(inputLines));
+    }
+
+    private static String part1(List<String> inputLines) {
+        List<Pair<Integer, Integer>> coordinates = InputUtils.readLinesAsCoordinates(inputLines, numberOfBytesPart1);
+        CharacterField cf = new CharacterField(maxXCoordinate + 1, maxYCoordinate + 1, ".");
+        for (Pair<Integer, Integer> blockingByte : coordinates) {
+            cf.setCharacterAt(BLOCKING, blockingByte);
+        }
+        //cf.prettyPrint();
+        Graph<Pair<Integer, Integer>, DefaultEdge> graph = CharacterFieldUtils.calculateUndirectedGraph(cf, BLOCKING);
+        BFSShortestPath<Pair<Integer, Integer>, DefaultEdge> bsfShortestPath = new BFSShortestPath<>(graph);
+        GraphPath<Pair<Integer, Integer>, DefaultEdge> path =
+                bsfShortestPath.getPath(new Pair<>(0, 0), new Pair<>(maxXCoordinate, maxYCoordinate));
+        long count = path.getLength();
+        System.out.println("\nPart 1 > Result: " + count);
+        return String.valueOf(count);
+    }
+
+    private static String part2(List<String> inputLines) {
+        List<Pair<Integer, Integer>> coordinates = InputUtils.readLinesAsCoordinates(inputLines);
+        CharacterField cf = new CharacterField(maxXCoordinate + 1, maxYCoordinate + 1, ".");
+        // from part 1 we know that there is a path for the first numberOfBytesPart1 blocking bytes
+        int i = 0;
+        while (i < numberOfBytesPart1) {
+            Pair<Integer, Integer> blockingByte = coordinates.get(i);
+            cf.setCharacterAt(BLOCKING, blockingByte);
+            i++;
+        }
+
+        String result = "";
+        while (i < coordinates.size()) {
+            // add one more byte
+            Pair<Integer, Integer> blockingByte = coordinates.get(i);
+            cf.setCharacterAt(BLOCKING, blockingByte);
+
+            Graph<Pair<Integer, Integer>, DefaultEdge> graph = CharacterFieldUtils.calculateUndirectedGraph(cf, BLOCKING);
+            BFSShortestPath<Pair<Integer, Integer>, DefaultEdge> bsfShortestPath = new BFSShortestPath<>(graph);
+            GraphPath<Pair<Integer, Integer>, DefaultEdge> path =
+                    bsfShortestPath.getPath(new Pair<>(0, 0), new Pair<>(maxXCoordinate, maxYCoordinate));
+            if (path == null) {
+                result = blockingByte.getValue0() + "," + blockingByte.getValue1();
+                break;
+            }
+            i++;
+        }
+
+        System.out.println("\nPart 2 > Result: " + result);
+        return result;
+    }
+
+}
---
+++ src/test/java/de/steffzilla/aoc/y2024/Aoc2024_18Test.java
@@ -0,0 +1,39 @@
+package de.steffzilla.aoc.y2024;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.steffzilla.aoc.AocUtils;
+import org.javatuples.Pair;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class Aoc2024_18Test {
+
+    static final String DAY = "18";
+
+    @Test
+    public void testSolveExample() {
+        List<String> lines = Aoc2024_18.initialize(false);
+        Pair<String, String> solutions = Aoc2024_18.solve(lines);
+        assertEquals("22", solutions.getValue0());
+        assertEquals("6,1", solutions.getValue1());
+    }
+
+    @Test
+    public void testSolvePuzzle() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("solutions.json");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(inputStream);
+
+        List<String> lines = Aoc2024_18.initialize(true);
+        Pair<String, String> solutions = Aoc2024_18.solve(lines);
+        assertEquals(root.at("/adventOfCode/2024/day" + DAY + "/part1").asText(), solutions.getValue0());
+        assertEquals(root.at("/adventOfCode/2024/day" + DAY + "/part2").asText(), solutions.getValue1());
+    }
+
+}
