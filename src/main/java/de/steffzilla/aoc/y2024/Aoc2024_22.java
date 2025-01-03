3a281cd093f25dbbeb92b93aa9016e1ca6603302
steffzilla@mail.com <steffzilla@mail.com>
2025-01-03 09:10:47 +0100

    Added the solution for part 1 of day 22 and tests

---
+++ src/main/java/de/steffzilla/aoc/y2024/Aoc2024_22.java
@@ -0,0 +1,90 @@
+package de.steffzilla.aoc.y2024;
+
+import de.steffzilla.aoc.InputUtils;
+import org.javatuples.Pair;
+
+import java.util.List;
+
+public class Aoc2024_22 {
+
+    private static final String DAY = "22";
+    private static final String YEAR = "2024";
+    private static final String USERNAME = System.getProperty("user.name");
+    public static final String BASEDIR = "C://Users//" + USERNAME + "//Downloads//AoC" + YEAR + "//";
+    public static final String INPUT_FILENAME = "input" + YEAR + "_" + DAY + ".txt";
+
+    static final String example = """
+            1
+            10
+            100
+            2024
+            """;
+
+    public static void main(String[] args) {
+        List<Long> inputLines = initialize(true);
+        solve(inputLines);
+    }
+
+    static List<Long> initialize(boolean solvePuzzle) {
+        List<Long> numbers;
+        if (solvePuzzle) {
+            System.out.println(DAY + ".12." + YEAR + ": Solving the PUZZLE");
+            // set global settings here
+            numbers = InputUtils.getListOfLongFromFile(BASEDIR + INPUT_FILENAME);
+        } else {
+            System.out.println(DAY + ".12." + YEAR + ": Solving the example");
+            numbers = example.lines().mapToLong(Long::valueOf).boxed().toList();
+            // set global settings here
+        }
+        return numbers;
+    }
+
+    static Pair<String, String> solve(List<Long> inputLines) {
+        return new Pair<>(part1(inputLines), part2(inputLines));
+    }
+
+    private static String part1(List<Long> numbers) {
+        long count = 0;
+        for (Long number : numbers) {
+            long result = number;
+            for (int i = 0; i < 2000; i++) {
+                result = calculateNextSecretNumber(result);
+            }
+            count+=result;
+        }
+        System.out.println("\nPart 1 > Result: " + count);
+        return String.valueOf(count);
+    }
+
+
+    private static String part2(List<Long> numbers) {
+        long count = 0;
+        for (Long number : numbers) {
+
+        }
+        System.out.println("\nPart 2 > Result: " + count);
+        return String.valueOf(count);
+    }
+
+    static long calculateNextSecretNumber(long secretNumber) {
+        long temp = secretNumber * 64;
+        secretNumber = mix(secretNumber, temp);
+        secretNumber = prune(secretNumber);
+        temp = (long) Math.floor((double) secretNumber / 32);
+        secretNumber = mix(secretNumber, temp);
+        secretNumber = prune(secretNumber);
+        temp = secretNumber * 2048;
+        secretNumber = mix(secretNumber, temp);
+        secretNumber = prune(secretNumber);
+        return secretNumber;
+    }
+
+    static long mix(long secretNumber, long otherNumber) {
+        // bitwise XOR
+        return secretNumber ^ otherNumber;
+    }
+
+    static long prune(long secretNumber) {
+        return secretNumber % 16777216;
+    }
+}
---
+++ src/test/java/de/steffzilla/aoc/y2024/Aoc2024_22Test.java
@@ -0,0 +1,72 @@
+package de.steffzilla.aoc.y2024;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.steffzilla.aoc.AocUtils;
+import de.steffzilla.aoc.InputUtils;
+import org.javatuples.Pair;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class Aoc2024_22Test {
+
+    static final String DAY = "22";
+    static final String sample = """
+            1
+            10
+            100
+            2024
+            """;
+
+    @Test
+    public void testMixAndPrune() {
+        assertEquals(37, Aoc2024_22.mix(42, 15));
+        assertEquals(1013579214, Aoc2024_22.mix(15887950, (15887950 * 64)));
+        assertEquals(1041709600, Aoc2024_22.mix(16495136, (16495136 * 64)));
+        assertEquals(16113920, Aoc2024_22.prune(100000000));
+        assertEquals(10195840, Aoc2024_22.prune(1016828800));
+        assertEquals(6946254, Aoc2024_22.prune(1013579214));
+    }
+
+    @Test
+    public void testCalculateNextSecretNumber() {
+        long nextNumber = Aoc2024_22.calculateNextSecretNumber(123);
+        assertEquals(15887950, nextNumber);
+
+        nextNumber = Aoc2024_22.calculateNextSecretNumber(15887950);
+        assertEquals(16495136, nextNumber);
+
+        nextNumber = Aoc2024_22.calculateNextSecretNumber(16495136);
+        assertEquals(527345, nextNumber);
+
+        nextNumber = 123;
+        for (int i = 0; i < 10; i++) {
+            nextNumber = Aoc2024_22.calculateNextSecretNumber(nextNumber);
+        }
+        assertEquals(5908254, nextNumber);
+    }
+
+    @Test
+    public void testSolveExample() {
+        Pair<String, String> solutions = Aoc2024_22.solve(sample.lines().mapToLong(Long::valueOf).boxed().toList());
+        assertEquals("37327623", solutions.getValue0());
+        //assertEquals("23", solutions.getValue1());
+    }
+
+    @Test
+    public void testSolvePuzzle() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("solutions.json");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(inputStream);
+
+        Pair<String, String> solutions =
+                Aoc2024_22.solve(InputUtils.getListOfLongFromFile(Aoc2024_22.BASEDIR + Aoc2024_22.INPUT_FILENAME));
+        assertEquals(root.at("/adventOfCode/2024/day" + DAY + "/part1").asText(), solutions.getValue0());
+        //assertEquals(root.at("/adventOfCode/2024/day" + DAY + "/part2").asText(), solutions.getValue1());
+    }
+
+}
