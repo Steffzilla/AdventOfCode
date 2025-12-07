b080b0e5979b20851357a55a8ce13e7cf2a43762
Steffzilla <steffzilla@mail.com>
2025-12-07 22:25:57 +0100

    Test for day 4 2025

---
+++ src/test/java/de/steffzilla/aoc/y2025/Aoc2025_04Test.java
@@ -0,0 +1,49 @@
+package de.steffzilla.aoc.y2025;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.steffzilla.aoc.AocUtils;
+import org.javatuples.Pair;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Aoc2025_04Test {
+
+    static final String DAY = "04";
+    static final String sample = """
+            ..@@.@@@@.
+            @@@.@.@.@@
+            @@@@@.@.@@
+            @.@@@@..@.
+            @@.@@@@.@@
+            .@@@@@@@.@
+            .@.@.@.@@@
+            @.@@@.@@@@
+            .@@@@@@@@.
+            @.@.@@@.@.
+            """;
+
+
+    @Test
+    public void testSolveExample() {
+        Pair<String, String> solutions = Aoc2025_04.solve(sample.lines().toList());
+        assertEquals("13", solutions.getValue0());
+        assertEquals("43", solutions.getValue1());
+    }
+
+    @Test
+    public void testSolvePuzzle() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("solutions.json");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(inputStream);
+
+        Pair<String, String> solutions = Aoc2025_04.solve(AocUtils.getStringList(Aoc2025_04.PATH));
+        assertEquals(root.at("/adventOfCode/2025/day" + DAY + "/part1").asText(), solutions.getValue0());
+        assertEquals(root.at("/adventOfCode/2025/day" + DAY + "/part2").asText(), solutions.getValue1());
+    }
+
+}
