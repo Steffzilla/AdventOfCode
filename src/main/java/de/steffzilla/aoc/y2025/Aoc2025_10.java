56645165f0c6edb4f88379ab7b6bb68c11943359
Steffzilla <steffzilla@mail.com>
2025-12-31 13:59:12 +0100

    Solution for part 2 of day 10 2025

--- src/main/java/de/steffzilla/aoc/y2025/Aoc2025_10.java
+++ src/main/java/de/steffzilla/aoc/y2025/Aoc2025_10.java
@@ -1,7 +1,13 @@
 package de.steffzilla.aoc.y2025;
 
 import de.steffzilla.aoc.AocUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.javatuples.Pair;
+import org.jspecify.annotations.NonNull;
+import org.ojalgo.optimisation.Expression;
+import org.ojalgo.optimisation.ExpressionsBasedModel;
+import org.ojalgo.optimisation.Optimisation;
+import org.ojalgo.optimisation.Variable;
 
 import java.util.*;
 import java.util.regex.Matcher;
@@ -26,7 +32,7 @@ public class Aoc2025_10 {
         System.out.println(DAY + ".12." + YEAR);
         //List<String> inputLines = example.lines().toList();
         List<String> inputLines =
-        AocUtils.getStringList(PATH);
+                AocUtils.getStringList(PATH);
 
         solve(inputLines);
     }
@@ -39,12 +45,32 @@ public class Aoc2025_10 {
             int lightTargetState = getLightTargetState(lightDiagram);
             String remainder = parts[1];
             parts = remainder.split(" \\{");
-            List<Integer> buttonWiringList = getButtonWiringList(lightDiagram.length(), parts[0]);
+            List<int[]> buttonWiringBinary = getButtonWiringBinary(lightDiagram.length(), parts[0]);
+
+            List<Integer> buttonWiringAsNumbers = getWiringAsNumbers(buttonWiringBinary);
+
             String joltageRequirements = parts[1].substring(0, parts[1].length() - 1);
+            String[] singleJoltages = joltageRequirements.split(",");
+            int[] numericJoltages = new int[singleJoltages.length];
+            for (int i = 0; i < singleJoltages.length; i++) {
+                numericJoltages[i] = Integer.parseInt(singleJoltages[i]);
+            }
             descriptions.add(
-                    new MachineDescription(lightTargetState, buttonWiringList, joltageRequirements));
+                    new MachineDescription(lightTargetState, buttonWiringAsNumbers, buttonWiringBinary, numericJoltages));
         }
         return new Pair<>(part1(descriptions), part2(descriptions));
+    }
+
+    static @NonNull List<Integer> getWiringAsNumbers(List<int[]> buttonWiringBinary) {
+        List<Integer> buttonWiringAsNumbers = new ArrayList<>();
+        for (int[] digits : buttonWiringBinary) {
+            StringBuilder sb = new StringBuilder();
+            for (int digit : digits) {
+                sb.append(digit);
+            }
+            buttonWiringAsNumbers.add(Integer.parseInt(sb.toString(), 2));
+        }
+        return buttonWiringAsNumbers;
     }
 
     static int getLightTargetState(String lightDiagram) {
@@ -53,49 +79,47 @@ public class Aoc2025_10 {
         return Integer.parseInt(lightDiagram, 2);
     }
 
-    static List<Integer> getButtonWiringList(int numberOfDigits, String buttonWiring) {
-        List<Integer> result = new ArrayList<>();
+    static List<int[]> getButtonWiringBinary(int numberOfDigits, String buttonWiring) {
+        List<int[]> buttonWiringBinary = new ArrayList<>();
         Pattern pattern = Pattern.compile("([\\d,]+)");
         Matcher matcher = pattern.matcher(buttonWiring);
-        int[] digits = new int[numberOfDigits];
 
         while (matcher.find()) {
+            int[] digits = new int[numberOfDigits];
             Arrays.fill(digits, 0);
             String buttonSpec = matcher.group(1);
             String[] split = buttonSpec.split(",");
             for (String digit : split) {
                 digits[Integer.parseInt(digit)] = 1;
             }
-            StringBuilder sb = new StringBuilder();
-            for (int digit : digits) {
-                sb.append(digit);
-            }
-            result.add(Integer.parseInt(sb.toString(), 2));
+            buttonWiringBinary.add(digits);
         }
-        return result;
+        return buttonWiringBinary;
     }
 
     private static String part1(List<MachineDescription> descriptions) {
         long result = 0;
         for (MachineDescription description : descriptions) {
-            result += getMinimalButtonPresses(description);
-//            System.out.println(result);
+            result += getMinimalButtonPressesPart1(description);
         }
         System.out.println("\nPart 1 > Result: " + result);
         return String.valueOf(result);
     }
 
-    private static long getMinimalButtonPresses(MachineDescription description) {
+    private static long getMinimalButtonPressesPart1(MachineDescription description) {
         int count = 0;
         boolean found = false;
         // check if target state == start state (all lights off)
         if (description.lightTargetState == 0) return count;
         Set<Integer> states = Set.of(0);
         do {
+            if (states.isEmpty()) {
+                throw new IllegalStateException("states should never be empty");
+            }
             count++;
             Set<Integer> newStates = new HashSet<>();
             for (Integer state : states) {
-                for (Integer buttonWiring : description.buttonWiringList) {
+                for (Integer buttonWiring : description.buttonWiringAsNumbers) {
                     newStates.add(state ^ buttonWiring);
                 }
             }
@@ -108,18 +132,72 @@ public class Aoc2025_10 {
         return count;
     }
 
-
     private static String part2(List<MachineDescription> descriptions) {
-        long count = 0;
+        long result = 0;
         for (MachineDescription description : descriptions) {
-
+            result += getMinimalButtonPressesPart2(description);
         }
-        System.out.println("\nPart 2 > Result: " + count);
-        return String.valueOf(count);
+        System.out.println("\nPart 2 > Result: " + result);
+        return String.valueOf(result);
     }
 
-    private record MachineDescription(int lightTargetState, List<Integer> buttonWiringList,
-                                      String joltageRequirements) {
+    private static long getMinimalButtonPressesPart2(MachineDescription description) {
+        long count = 0;
+        boolean found = false;
+        int length = description.joltageRequirements.length;
+        int[] initialState = new int[length];
+        Arrays.fill(initialState, 0);
+        // check if target state == start state (all lights off)
+        if (Arrays.equals(description.joltageRequirements, initialState)) return count;
+
+        count = solveEquations(description.joltageRequirements, description.buttonWiringBinary);
+        return count;
+    }
+
+    static long solveEquations(int[] joltageRequirements, List<int[]> reformattedButtonWiringList) {
+        ExpressionsBasedModel model = new ExpressionsBasedModel();
+        List<Variable> variables = new ArrayList<>();
+        //(1,3) (1,2) (0,1,2,3) {19,46,28,37}
+        for (int i = 0; i < reformattedButtonWiringList.size(); i++) {
+            variables.add(model.addVariable("v" + i).integer(true).lower(0));
+            //System.out.println(variables.get(i));
+        }
+        for (int i = 0; i < joltageRequirements.length; i++) {
+            Expression eq = model.addExpression("eq" + i).level(joltageRequirements[i]);
+            for (int j = 0; j < reformattedButtonWiringList.size(); j++) {
+                int[] button = reformattedButtonWiringList.get(j);
+                if (button[i] > 0) {
+                    eq.set(variables.get(j), button[i]);
+                }
+            }
+        }
+        Expression objective = model.addExpression("obj").weight(1.0);
+        for (Variable v : variables) {
+            objective.set(v, 1);
+        }
+        Optimisation.Result result = model.minimise();
+        if (!result.getState().isOptimal()) {
+            throw new IllegalStateException("Result state needs to be optimal! " + result.getState());
+        }
+        double value = result.getValue();
+        return Math.round(value);
+    }
+
+    static List<int[]> reformatButtonWiringList(List<Integer> buttonWiringList, int length) {
+        List<int[]> bW = new ArrayList<>();
+        for (int buttonWiring : buttonWiringList) {
+            String binaryString = StringUtils.leftPad(Integer.toBinaryString(buttonWiring), length, '0');
+            int[] bits = new int[binaryString.length()];
+            for (int i = 0; i < binaryString.length(); i++) {
+                bits[i] = Character.getNumericValue(binaryString.charAt(i));
+            }
+            bW.add(bits);
+        }
+        return bW;
+    }
+
+    private record MachineDescription(int lightTargetState, List<Integer> buttonWiringAsNumbers,
+                                      List<int[]> buttonWiringBinary, int[] joltageRequirements) {
     }
 
 }
--- src/test/java/de/steffzilla/aoc/y2025/Aoc2025_10Test.java
+++ src/test/java/de/steffzilla/aoc/y2025/Aoc2025_10Test.java
@@ -8,9 +8,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Aoc2025_10Test {
 
@@ -20,7 +21,7 @@ public class Aoc2025_10Test {
     void testSolveExample() {
         Pair<String, String> solutions = Aoc2025_10.solve(Aoc2025_10.example.lines().toList());
         assertEquals("7", solutions.getValue0());
-        //assertEquals("x", solutions.getValue1());
+        assertEquals("33", solutions.getValue1());
     }
 
     @Test
@@ -31,39 +32,25 @@ public class Aoc2025_10Test {
 
         Pair<String, String> solutions = Aoc2025_10.solve(AocUtils.getStringList(Aoc2025_10.PATH));
         assertEquals(root.at("/adventOfCode/2025/day" + DAY + "/part1").asText(), solutions.getValue0());
-        //assertEquals(root.at("/adventOfCode/2025/day" + DAY + "/part2").asText(), solutions.getValue1());
-    }
-
-    @Test
-    void getButtonWiringList() {
-        assertEquals(List.of(Integer.parseInt("0001", 2)),
-                Aoc2025_10.getButtonWiringList(4,"(3)"));
-        List<Integer> results = List.of(
-                Integer.parseInt("0001", 2),
-                Integer.parseInt("0101", 2),
-                Integer.parseInt("0010", 2),
-                Integer.parseInt("0011", 2),
-                Integer.parseInt("1010", 2),
-                Integer.parseInt("1100", 2)
-                );
-        assertEquals(results,
-                Aoc2025_10.getButtonWiringList(4,"(3) (1,3) (2) (2,3) (0,2) (0,1)"));
-        results = List.of(
-                Integer.parseInt("111110", 2),
-                Integer.parseInt("100110", 2),
-                Integer.parseInt("111011", 2),
-                Integer.parseInt("011000", 2)
-        );
-        assertEquals(results,
-                Aoc2025_10.getButtonWiringList(6,"(0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2)"));
+        assertEquals(root.at("/adventOfCode/2025/day" + DAY + "/part2").asText(), solutions.getValue1());
     }
 
     @Test
     void getLightTargetState() {
-        assertEquals(Integer.parseInt("0110",2), Aoc2025_10.getLightTargetState(".##."));
-        assertEquals(Integer.parseInt("00010",2), Aoc2025_10.getLightTargetState("...#."));
-        assertEquals(Integer.parseInt("011101",2), Aoc2025_10.getLightTargetState(".###.#"));
-        assertEquals(Integer.parseInt("111111", 2),Aoc2025_10.getLightTargetState("######"));
-        assertEquals(Integer.parseInt("1000010111", 2),Aoc2025_10.getLightTargetState("#....#.###"));
+        assertEquals(Integer.parseInt("0110", 2), Aoc2025_10.getLightTargetState(".##."));
+        assertEquals(Integer.parseInt("00010", 2), Aoc2025_10.getLightTargetState("...#."));
+        assertEquals(Integer.parseInt("011101", 2), Aoc2025_10.getLightTargetState(".###.#"));
+        assertEquals(Integer.parseInt("111111", 2), Aoc2025_10.getLightTargetState("######"));
+        assertEquals(Integer.parseInt("1000010111", 2), Aoc2025_10.getLightTargetState("#....#.###"));
     }
+
+    @Test
+    void testGetWiringAsNumbers() {
+        List<int[]> binary = new ArrayList<>();
+        binary.add(new int[]{0, 1, 0, 1});
+        binary.add(new int[]{1, 1, 1, 1});
+        binary.add(new int[]{0, 0, 1});
+        assertEquals(List.of(5, 15, 1), Aoc2025_10.getWiringAsNumbers(binary));
+    }
+
 }
